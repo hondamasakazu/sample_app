b4bbb4bc71adcb7e83525fa49824fc7927228e22
KoganezawaRyouta <ryouta_koganezawa@fexd.co.jp>
2013-11-28 16:57:08 +0900

    Rspec rifact

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 
 	# ページごとの完全なタイトルを返します。
 	def full_title(page_title)
-	    base_title = "Ruby on Rails Tutorial Sample App"
+	    base_title = "Network Solution Group"
 	    if page_title.empty?
-	    		base_title
+	    	base_title
 	    else
 	    	"#{base_title} | #{page_title}"
 	    end
