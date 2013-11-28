c27fafd581cca575566751b1421f7e79d0fc99eb
KoganezawaRyouta <ryouta_koganezawa@fexd.co.jp>
2013-11-28 15:57:53 +0900

    heroku first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  root :to => 'static_pages#home'
+  match '/static_pages/home',    to: 'static_pages#home',    via: 'get', as: 'home'
+  match '/static_pages/help',    to: 'static_pages#help',    via: 'get', as: 'help'
+  match '/static_pages/about',   to: 'static_pages#about',   via: 'get', as: 'about'
+  match '/static_pages/contact', to: 'static_pages#contact', via: 'get', as: 'contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "StaticPages" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('Future-Commynity')
     end
    it "should have the base title" do
      visit '/static_pages/home'
