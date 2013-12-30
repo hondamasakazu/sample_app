bced8dc92a68ff5d69afa3aa79f8a0081bdf7f70
KoganezawaRyouta <ryouta_koganezawa@fexd.co.jp>
2013-12-30 22:35:12 +0900

    default_url_optionsの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ SampleApp::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => "nsg-future-commynity.herokuapp.com/" }
+  config.action_mailer.default_url_options = { :host => "nsg-future-commynity.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
