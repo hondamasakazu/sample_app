f2ff8f96e3080cd7138f27dd644a99f681a37697
KoganezawaRyouta <ryouta_koganezawa@fexd.co.jp>
2013-12-30 22:30:22 +0900

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,14 +67,14 @@ SampleApp::Application.configure do
   config.action_mailer.default_url_options = { :host => "nsg-future-commynity.herokuapp.com/" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => 'smtp.gmail.com',
-    :port => '587',
-    :domain => 'smtp.gmail.com',
-    :authentication => 'plain',
-    :user_name => 'rubytest123456@gmail.com',
-    :password => 'kogane5513'
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
