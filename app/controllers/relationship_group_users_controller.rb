ad72c8de82ed9185b97b11036c0bad10fbe5ba47
KoganezawaRyouta <ryouta_koganezawa@fexd.co.jp>
2014-01-11 21:48:23 +0900

    グループメンバー追加

--- app/controllers/relationship_group_users_controller.rb
+++ app/controllers/relationship_group_users_controller.rb
@@ -5,7 +5,7 @@ class RelationshipGroupUsersController < ApplicationController
     @group = Group.find(params[:relationship_group_user][:group_id])
     @user = User.find(params[:relationship_group_user][:user_id])
     @user.join_groups!(@group)
-    flash.now[:success] = "Group created!"
+    flash[:success] = "#{@group.name}へ#{@user.name}さんを追加しました。"
     redirect_to @group
   end
 
@@ -15,5 +15,27 @@ class RelationshipGroupUsersController < ApplicationController
     @user.unjoin_groups!(@group)
     redirect_to @group
   end
+=begin
+Ajax対応したみたけど、上手くいかない。。。一旦コメントアウト
 
+  def create
+    @group = Group.find(params[:relationship_group_user][:group_id])
+    @user = User.find(params[:relationship_group_user][:user_id])
+    @user.join_groups!(@group)
+    respond_to do |format|
+      format.html { redirect_to @group }
+      format.js
+    end
+  end
+
+  def destroy
+    @group  = RelationshipGroupUser.find(params[:id]).group
+    @user = User.find(params[:relationship_group_user][:user_id])
+    @user.unjoin_groups!(@group)
+    respond_to do |format|
+      format.html { redirect_to @group }
+      format.js
+    end
+  end
+=end
 end
