5f161afffaf5b3a8f418969e183d304a3b12f30d
KoganezawaRyouta <ryouta_koganezawa@fexd.co.jp>
2014-03-22 19:03:00 +0900

    test

--- config/unicorn/conf.rb
+++ config/unicorn/conf.rb
@@ -8,19 +8,52 @@ if ENV['RAILS_ENV'] == "development"
   #stderr_path "#{proj_root_dir}/sample_app/log/unicorn.log"
   #stdout_path "#{proj_root_dir}/sample_app/log/unicorn.log"
 
-  app_path = '/var/www/capistrano.test'
-  app_shared_path = "#{app_path}/shared"
+  # app_path = '/var/www/capistrano.test'
+  # app_shared_path = "#{app_path}/shared"
   # 実態は symlink。SIGUSR2 を送った時にこの symlink に対して
   # Unicorn のインスタンスが立ち上がる
-  working_directory "#{app_path}/current/"
+  # working_directory "#{app_path}/current/"
 
-  worker_processes 4
+  # worker_processes 4
   # ダウンタイムをなくす
-  preload_app true
+  # preload_app true
+  # listen "#{app_shared_path}/tmp/sockets/unicorn.sock"
+  # pid "#{app_shared_path}/tmp/pids/unicorn.pid"
+  # stdout_path "#{app_shared_path}/log/unicorn.stdout.log"
+  # stderr_path "#{app_shared_path}/log/unicorn.stderr.log"
+
+
+  app_path = '/var/www/capistrano.test'
+  app_shared_path = "#{app_path}/shared"
+  worker_processes 5
+  # 実態は symlink。
+  # SIGUSR2 を送った時にこの symlink に対して
+  # Unicorn のインスタンスが立ち上がる
+  working_directory "#{app_path}/current/"
   listen "#{app_shared_path}/tmp/sockets/unicorn.sock"
-  pid "#{app_shared_path}/tmp/pids/unicorn.pid"
   stdout_path "#{app_shared_path}/log/unicorn.stdout.log"
   stderr_path "#{app_shared_path}/log/unicorn.stderr.log"
+  pid "#{app_shared_path}/tmp/pids/unicorn.pid"
+  # ダウンタイムをなくす
+  preload_app true
+  before_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+    old_pid = "#{ server.config[:pid] }.oldbin"
+    unless old_pid == server.pid
+      begin
+        Process.kill :QUIT, File.read(old_pid).to_i
+      rescue Errno::ENOENT, Errno::ESRCH
+      end
+    end
+  end
+
+  after_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  end
+
+
+
 
 end
 
